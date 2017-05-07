d6bbe112587f0888fa38cfc62cae1d6eade55e8b
andrey.antoniuk <andrey.antoniuk@instinctools.com>
2017-05-07 11:50:21 +0300

    R-2: integrated chai

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-describe('Test', function() {
-  it('first test', function() {
-    console.log(1);
+const { expect } = require('chai');
+
+describe('Test', () => {
+  it('first test', () => {
+    const a = 1;
+    expect(a).to.be.equal(1);
   });
 });
